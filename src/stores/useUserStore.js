bbdeb7ae32a24850fa482fe1cd9488d89c5a95eb
Joaquin Sosa <cto-joaquinsosa@hotmail.com>
2024-10-29 17:21:05 -0300

    add home view and user store logic

---
+++ src/stores/useUserStore.js
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore('user', {
+    state: () => ({
+        name: localStorage.getItem('userName') || "",
+        money: 1000
+    }),
+    actions: {
+        setUser(name) {
+            this.name = name;
+            localStorage.setItem('userName', name);
+        },
+        purchaseItem(price) {
+            this.money = this.money - price;
+        }
+    }
+})
